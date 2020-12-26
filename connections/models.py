cb657ca4a7537dc7d3bd4f5d369e4f0b60082ce4
vbukhonov <bukhonovvyu@gmail.com>
2020-12-26 17:16:58 +0300

    Add models and create migration.

--- connections/models.py
+++ connections/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ConnectedDev(models.Model):
+    twitter_id = models.CharField(max_length=100, null=False, default="")
+    twitter_username = models.CharField(max_length=100, null=False, default="")
+    github_id = models.CharField(max_length=100, null=False, default="")
+    github_username = models.CharField(max_length=100, null=False, default="")
+
+
+class RequestRecord(models.Model):
+    registered_at = models.DateTimeField()
+    dev_1 = models.ForeignKey(to=ConnectedDev, on_delete=models.CASCADE, null=False, related_name="dev_1")
+    dev_2 = models.ForeignKey(to=ConnectedDev, on_delete=models.CASCADE, null=False, related_name="dev_2")
+    connected = models.BooleanField()
+    common_organizations = models.TextField(max_length=100, default="[]", null=False)
